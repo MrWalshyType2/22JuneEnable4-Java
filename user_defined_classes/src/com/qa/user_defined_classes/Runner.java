844bfa8146a0aafa2b6916d431676e1f9325ab68
MrWalshyType2 <mwalsh@qa.com>
2022-07-07 15:22:26 +0100

    Added getters and setters to the example

--- user_defined_classes/src/com/qa/user_defined_classes/Runner.java
+++ user_defined_classes/src/com/qa/user_defined_classes/Runner.java
@@ -12,14 +12,17 @@ public class Runner {
 		Car otherCar = new Car();
 		
 		// we can use dot notation to access the data on the car object
-		System.out.println(car.manufacturer); // null
+		System.out.println(car.getManufacturer()); // null
 		// car.manufacturer is null because we haven't initialised it
 		// with a different value
 		System.out.println(car.numOfDoors); // 0
 		
 		// we can also change the value of an instance variable
-		car.manufacturer = "Mazda";
-		System.out.println(car.manufacturer); // "Mazda"
+		car.setManufacturer("Mazda");
+		car.numOfSeats = 2;
+		car.engineSize = 355f;
+		
+		System.out.println(car.getManufacturer()); // "Mazda"
 		
 		// We can call displayInfo on an instance (object) of Car
 		// but we cannot call it directly through the class
--- user_defined_classes/src/com/qa/user_defined_classes/model/Car.java
+++ user_defined_classes/src/com/qa/user_defined_classes/model/Car.java
@@ -1,7 +1,7 @@
 package com.qa.user_defined_classes.model;
 
 public class Car {
-
+	
 	// the state/data that belongs to an object created from a class
 	// is declared at the class-level
 	// - this state is referred to as the fields of a class in Java
@@ -12,9 +12,12 @@ public class Car {
 	
 	// Fields are structured as follows:
 	// - AccessModifier DataType fieldName;
-	public String manufacturer; // null
+	// - private fields and methods cannot be accessed outside of this class
+	private String manufacturer; // null
 	public String model; // null
 	public byte numOfDoors; // 0
+	public float engineSize;
+	public int numOfSeats;
 	
 	// These fields are referred to as instance variables, this is because
 	// we can only use these variables if we have an object to use them
@@ -24,8 +27,29 @@ public class Car {
 	public void displayInfo() { // doesn't take inputs, no parameters
 		System.out.println("Manufacturer: " + this.manufacturer);
 		System.out.println("Model: " + this.model);
-		System.out.println("Doors: " + this.numOfDoors + "\n");
+		System.out.println("Doors: " + this.numOfDoors);
+		System.out.println("Engine size: " + this.engineSize);
+		System.out.println("Number of seats: " + this.numOfSeats + "\n");
 		// 'this' refers to the object that you are calling the method on
 	}
 	
+	// We can return data from a method
+	// - this is a getter/accessor
+	public String getManufacturer() {
+		// we return data from a method using the return keyword
+		// - return someData;
+		return this.manufacturer;
+	}
+	
+	// We can also accept inputs to set data
+	// - this is a setter/mutator
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+		// any variable within a method, or code block, that is not
+		// at the class-level is a local variable
+		// - local variables can only be accessed within the scope (code block)
+		//   that they are declared in, or any nested code blocks in the same
+		//   file
+		// - local variables cannot have access modifiers
+	}
 }

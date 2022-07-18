adad6e13fc4dabd0f4237465f020e3a7b25feafc
MrWalshyType2 <mwalsh@qa.com>
2022-07-18 15:20:27 +0100

    Added JUnit example

---
+++ junit/src/main/java/com/qa/examples/junit/Calculator.java
@@ -0,0 +1,8 @@
+package com.qa.examples.junit;
+
+public class Calculator {
+
+	public double add(double num1, double num2) {
+		return num1 + num2;
+	}
+}
---
+++ junit/src/test/java/com/qa/examples/junit/CalculatorUnitTest.java
@@ -0,0 +1,47 @@
+package com.qa.examples.junit;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class CalculatorUnitTest {
+
+	private Calculator calculator;
+
+	// usually used for configuring a db in an integration test for example, configuring the class
+	// ready for all the tests to run
+	@BeforeAll // methods marked @BeforeAll run before all tests in this file run
+	public static void setup() {
+		System.out.println("Before all tests");
+	}
+
+	@BeforeEach // runs before each test, useful for configuring test data
+	public void init() {
+		System.out.println("Before a test");
+		calculator = new Calculator();
+	}
+
+	@AfterAll // methods marked @AfterAll run after all tests in this file run
+	public static void done() {
+		System.out.println("After all tests");
+	}
+
+	@AfterEach // runs after each test
+	public void tearDown() {
+		System.out.println("After a test");
+	}
+	
+	@Test // marks this method as a test
+	public void addTest() {
+		double num1 = 32;
+		double num2 = 50;
+		double expected = 82;
+		double actual = calculator.add(num1, num2);
+		
+		// use a assertion method built into JUnit to check the results where as expected
+		Assertions.assertEquals(expected, actual);
+	}
+}
